b7f554528e7168407b1523ad5403453773f89c9c
Vano4ok <pivtorak.vn4k@gmail.com>
2023-03-11 22:35:58 +0200

    Add project files.

---
+++ src/VeeArc.WebAPI/Program.cs
@@ -0,0 +1,28 @@
+using VeeArc.Application;
+using VeeArc.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddApplication(builder.Configuration);
+builder.Services.AddInfrastructure(builder.Configuration);
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
