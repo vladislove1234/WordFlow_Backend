07b225f170e695d91289b5ababeb1981d78d6865
Vano4ok <pivtorak.vn4k@gmail.com>
2023-03-15 11:49:42 +0200

    Added ExceptionFilter, NotFoundException, ValidationException

--- src/VeeArc.WebAPI/Program.cs
+++ src/VeeArc.WebAPI/Program.cs
@@ -1,12 +1,16 @@
 using VeeArc.Application;
 using VeeArc.Infrastructure;
+using VeeArc.WebAPI.Filter;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddApplication(builder.Configuration);
 builder.Services.AddInfrastructure(builder.Configuration);
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options =>
+{
+    options.Filters.Add<ExceptionFilter>();
+});
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
